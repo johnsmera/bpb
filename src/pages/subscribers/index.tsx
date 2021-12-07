9ed8044e62700e379ab997a37006f738c2aad19f
John Smera <johnsmera@live.com>
2021-12-07 17:39:30 -0300

    feat: init subscribers page

---
+++ src/pages/subscribers/index.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next'
+import { Container } from '../../styles/Subscriber.module'
+
+const Subscribers: NextPage = () => {
+  return <Container>Hello world</Container>
+}
+
+export default Subscribers
